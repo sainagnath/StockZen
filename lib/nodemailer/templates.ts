f06a3162ff8ecbfad5e0e53556ba399bd114fc43
Sai Nath <bongursainath@gmail.com>
2025-12-18 00:24:13 +0530

    Added Daily News Summary

--- lib/nodemailer/templates.ts
+++ lib/nodemailer/templates.ts
@@ -95,11 +95,7 @@ export const WELCOME_EMAIL_TEMPLATE = `<!DOCTYPE html>
                 <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" class="email-container" style="max-width: 600px; background-color: #141414; border-radius: 8px; border: 1px solid #30333A;">
                     
                     <!-- Header with Logo -->
-                    <tr>
-                        <td align="left" class="mobile-header-padding" style="padding: 40px 40px 20px 40px;">
-                            <img src="https://ik.imagekit.io/a6fkjou7d/logo.png?updatedAt=1756378431634" alt="Signalist Logo" width="150" style="max-width: 100%; height: auto;">
-                        </td>
-                    </tr>
+
                     
                     <!-- Dashboard Preview Image -->
                     <tr>
